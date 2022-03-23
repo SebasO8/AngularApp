42061f17da5cf770d892061b13477965278c3f62
Sebastian-Ocampo-Lopez <sebastianocampolopez507@gmail.com>
2022-03-22 22:02:55 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserComponent } from './components/user/user.component';
+import { FormUserComponent } from './components/form-user/form-user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//materialize components
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    FooterComponent,
+    UserComponent,
+    FormUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
